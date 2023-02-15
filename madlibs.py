1704d364ac66dd4724ea339bf813a0253a15974d
Sahala Josua Sinaga <sahalajosuasinaga@gmail.com>
2023-02-15 23:35:13 +0700

    last commit

---
+++ madlibs.py
@@ -0,0 +1,21 @@
+"""
+    I learn this code from "Kylie Ying" - YouTube Channel: https://www.youtube.com/ycubed
+    name    : Sahala Josua Sinaga
+    email   : sahalajosuasinaga@gmail.com
+    
+    MadLibs
+"""
+
+
+## String concatenation (aka how to put strings togerher)
+
+
+adj = input("Adjective: ")
+verb1 = input("Verb1: ")
+verb2 = input("Verb2: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
